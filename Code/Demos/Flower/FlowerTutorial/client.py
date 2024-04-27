2a14a0ea022d8029754f32511ef94625b7a18ab1
ofithcheallaigh <sofithcheallaigh@gmail.com>
2024-04-27 23:34:33 +0100

    Updating client code to deal with local training and updates

--- Code/Demos/Flower/FlowerTutorial/client.py
+++ Code/Demos/Flower/FlowerTutorial/client.py
@@ -1,5 +1,9 @@
+from typing import Dict
 import flwr as fl
+from flwr.common import NDArrays
 import torch
+
+from model import Net, train, test
 
 class FlowerClient(fl.client.NumPyClient):
     def __init__(self,
@@ -22,6 +26,23 @@ class FlowerClient(fl.client.NumPyClient):
 
         self.model.load_state_dict(state_dict, strict=True)
 
+    # Extracts list from model
+    def get_parameters(self, config: Dict[str, Scalar]):
+        
+        return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
+
+
     def fit(self, parameters, config):
         # Copy the parameters sent by the server into the client's local model
         self.set_parameters(parameters)
+
+        lr = config['lr']
+        momentum = config['momentum']
+        epochs = config['local_epochs']
+
+        optim = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
+
+        # Do local training
+        train(self.model, self.trainload, optim, epochs, self.device) # Model no longer has weights sent my the server
+
+        return self.get_parameters(), len(self.trainload), {}
